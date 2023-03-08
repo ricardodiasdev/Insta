21c89ece0ae186b07025377fd60cdb8be44cd3a0
Ricardo Dias <ricardodias.dev@gmail.com>
2023-03-08 16:25:03 -0300

    Página inicial e componente de vizualização das Feeds

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+
+
+import Lista from "./src/components/Lista";
+
+import feed from "./src/data/Feed";
+
+import logo from "./src/img/logo.png";
+import send from "./src/img/send.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-     
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Image source={logo} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image source={send} style={styles.send} />
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        data={feed}
+        renderItem={( {item} ) => <Lista data={item}/>}
+      />
     </View>
   );
 }
@@ -11,8 +38,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  header: {
+    height: 55,
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: 50,
+    padding: 5,
+    borderBottomWidth: 0.2,
+    shadowColor: "#000",
+    elevation: 1,
+  },
+  send: {
+    width: 23,
+    height: 23,
   },
 });
---
+++ src/components/Lista/index.js
@@ -0,0 +1,13 @@
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React, {useState} from "react";
+
+const Lista = (props) => {
+const {data} = props
+  return (
+    <View>
+      <Text> Lista</Text>
+    </View>
+  );
+};
+
+export default Lista;
